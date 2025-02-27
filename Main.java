159c0bf3da7099a6504358a51983012d964b9bbf
DDDlivp <weimeng2739@gmail.com>
2025-02-27 23:38:46 +0800

    Moved Java files into HolidayTravelVehicles directory

--- Main.java
+++
@@ -1,8 +0,0 @@
-package HolidayTravelVehicles;
-
-public class Main {
-    public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle("V001", "SN12345", "Toyota Camry", "XLE", 2023, "Toyota", 30000.0);
-        vehicle.displayVehicleInfo();
-    }
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-# 2024-ITCS431-Gemini13
-6388175 Zishu Wang
-
-ITCS431 Software Design and Development (2024)

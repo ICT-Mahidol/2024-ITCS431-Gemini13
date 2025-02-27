f9b03031510c9120c1237678eedeb6e93a91ba2d
DDDlivp <weimeng2739@gmail.com>
2025-02-27 23:51:36 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package HolidayTravelVehicles;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle("V001", "SN12345", "Toyota Camry", "XLE", 2023, "Toyota", 30000.0);
+        vehicle.displayVehicleInfo();
+    }
+}

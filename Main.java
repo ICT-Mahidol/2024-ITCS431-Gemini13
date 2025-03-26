90e57de7e2788981c3f0c719f2fb5c740f3a0943
DDDlivp <weimeng2739@gmail.com>
2025-03-26 16:57:18 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,28 @@
-package HolidayTravelVehicles;
+package com.directi.training.codesmells;
 
-public class Main {
-    public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle("V001", "SN12345", "Toyota Camry", "XLE", 2023, "Toyota", 30000.0);
-        vehicle.displayVehicleInfo();
+import com.directi.training.codesmells.smelly.chess.GameEngine;
+import com.directi.training.codesmells.smelly.chess.Player;
+
+import java.util.Scanner;
+
+public class Main
+{
+    private static final Scanner scanner = new Scanner(System.in);
+    private static GameEngine gameEngine;
+
+    public static void main(String[] args)
+    {
+        initGameEngine();
+        gameEngine.initGame();
+        gameEngine.startGame();
+    }
+
+    private static void initGameEngine()
+    {
+        System.out.print("Enter Player 1 Name: ");
+        Player player1 = new Player(scanner.nextLine());
+        System.out.print("Enter Player 2 Name: ");
+        Player player2 = new Player(scanner.nextLine());
+        gameEngine = new GameEngine(player1, player2);
     }
 }

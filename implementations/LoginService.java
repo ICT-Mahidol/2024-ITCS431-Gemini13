c6c76dfedeb2738fd4201f4ee617e0928c65d5e2
DDDlivp <weimeng2739@gmail.com>
2025-05-22 13:23:01 +0800

    Create LoginService.java

---
+++ implementations/LoginService.java
@@ -0,0 +1,17 @@
+package com.gemini13.service;
+
+import com.gemini13.model.User;
+import com.gemini13.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public boolean login(String username, String password) {
+        User user = userRepository.findByUsernameAndPassword(username, password);
+        return user != null;
+    }
+}

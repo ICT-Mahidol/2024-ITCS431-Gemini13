306f92af594a79743dd73cc91e790b3648711f6a
DDDlivp <weimeng2739@gmail.com>
2025-05-22 13:30:50 +0800

    Update Gemini13Application.java

--- implementations/Gemini13Application.java
+++ implementations/Gemini13Application.java
@@ -1,1 +1,12 @@
+package com.gemini13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gemini13Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Gemini13Application.class, args);
+    }
+}
 

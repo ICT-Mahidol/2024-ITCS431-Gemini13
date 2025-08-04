e6a0fcf4c8e5afc89ab9ae4d4973170a26911a56
DDDlivp <weimeng2739@gmail.com>
2025-08-04 21:20:16 +0800

    Delete implementations directory

--- implementations/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gemini13.repository;
-
-import com.gemini13.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsernameAndPassword(String username, String password);
-}
-
